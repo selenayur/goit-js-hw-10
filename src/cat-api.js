9893f405893273eafda15def56daa600e3d41f92
selenayur <125310718+selenayur@users.noreply.github.com>
2023-07-23 00:52:45 +0300

    finish

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,25 +1,15 @@
-const BASE_URL = 'https://api.thecatapi.com/v1';
-const API_KEY = 'live_b81K7BzuDEA8swj2RedgfBjdwI60KdZF8WX37AknRgz0KNZAvL9l6pTURqzcmHR9';
-               
+import axios from "axios";
 
-export function fetchBreeds() {
-  return fetch(
-    `${BASE_URL}/breeds`
-  ).then( resp => {
-    if ( !resp.ok ) {
-      throw new Error(resp.statusText)
-    }  
-    return resp.json()
-  } )
-}
+axios.defaults.headers.common["x-api-key"] = 'live_b81K7BzuDEA8swj2RedgfBjdwI60KdZF8WX37AknRgz0KNZAvL9l6pTURqzcmHR9';
 
-export function fetchCatByBreed( breedId ) {
-  return fetch(
-    `${BASE_URL}/images/search?limit=10&breed_ids=${breedId}&api_key=${API_KEY}`
-  ).then( resp => {
-    if ( !resp.ok ) {
-      throw new Error(resp.statusText)
-    }  
-    return (resp.json())
-  } )
+const BASE_URL = 'https://api.thecatapi.com/v1/';
+const END_POINT_BREEDS = 'breeds';
+const END_POINT_SEARCH = 'images/search';
+
+export  function fetchBreeds() {
+    return axios.get(`${BASE_URL}${END_POINT_BREEDS}`);    
+};
+export function fetchCatByBreed(breedId) {
+    const url = `${BASE_URL}${END_POINT_SEARCH}?breed_ids='${breedId}'`
+    return axios.get(`${BASE_URL}${END_POINT_SEARCH}?breed_ids=${breedId}`);
 }
--- src/index.js
+++ src/index.js
@@ -1,80 +1,69 @@
-import { fetchBreeds, fetchCatByBreed } from "./cat-api";
-import SlimSelect from 'slim-select';
+import { fetchBreeds, fetchCatByBreed }  from './cat-api'
+import SlimSelect from 'slim-select'
 import 'slim-select/dist/slimselect.css';
 import Notiflix from 'notiflix';
-
-const elements = {
-    selectCat: document.querySelector('.breed-select'),
-    catInfo: document.querySelector('.cat-info'),
+const refs = {
+    selected: document.querySelector('.breed-select'),
     loader: document.querySelector('.loader'),
-    error: document.querySelector('.error')
+    error: document.querySelector('.error'),
+    div: document.querySelector('.cat-info'),
+    option: document.querySelector('.breed-select option')
 }
+refs.selected.classList.add('hidden');
+refs.option.setAttribute('selected', 'selected');
+refs.option.setAttribute('disabled', 'disabled');
+refs.option.textContent = 'Please selected breeds cat';
+refs.selected.setAttribute("id", "slim-select");
+refs.selected.addEventListener('change', onSelect);
+
+function onSelect(event) {
+    refs.loader.classList.remove('hidden');
+     refs.div.innerHTML = "";
+    fetchCatByBreed(event.target.value)
+         .then((response) => {
+             makeMarkCard(response.data[0].breeds[0], response.data[0]);
+           refs.loader.classList.add('hidden'); 
+         }).catch((error) => {
+            Notiflix.Notify.failure('Error getting data. Please try again later.');
+            refs.loader.classList.add('hidden');
+
+        }).finally(); 
 
 
+};
 
-elements.catInfo.classList.add('is-hidden');
-elements.loader.classList.replace('is-hidden', 'loader');
-elements.error.classList.add("is-hidden");
-
-fetchBreeds()
-    .then(data => {
-    elements.loader.classList.replace('loader', 'is-hidden');
-
-    console.log(data)
-        const listAllCats = data.map(({ id, name }) => `<option value="${id}">${name}</option>`).join('');
-        elements.selectCat.insertAdjacentHTML("beforeend", listAllCats)
-
-    new SlimSelect({
-        select: 'elements.selectCat'
-    })
-})
-
-elements.selectCat.addEventListener('change', onSelectCat);
-
-function onSelectCat(e) {
-    elements.loader.classList.replace('is-hidden', 'loader');
-    elements.selectCat.classList.remove('is-hidden');
-    elements.catInfo.classList.add('is-hidden');
-    elements.error.classList.add('is-hidden');
-
-    const breedId = e.target.value;
-
-    fetchCatByBreed(breedId)
-        .then(data => {
-    elements.loader.classList.replace('loader', 'is-hidden');
-    elements.selectCat.classList.remove('is-hidden');
-    elements.catInfo.classList.remove('is-hidden')
-    elements.error.classList.add('is-hidden');
-
-
-            let { url, breeds } = data[0];
-            console.log(data[0]);
-            elements.catInfo.innerHTML = `
-        <div class="container-info">
-        <img src="${url}" alt="${breeds[0].name}" width="400">
-        <div class="text-information">
-        <h2 class="title">${breeds[0].name}</h2>
-        <p class="description">${breeds[0].description}</p>
-        <h3 class="character">Temperament: <span class="text_temp">${breeds[0].temperament}</span></h3>
-        </div>
-        </div>`
+fetchBreeds().then((response) => {
+   
+    makeMarkOptions(response.data)
+     new SlimSelect({
+            select: '#slim-select',
     
-        elements.loader.classList.replace('loader', 'is-hidden');
-    })
-    .catch(onError)
-}
-
-function onError(error) {
-    elements.loader.classList.replace('loader', 'is-hidden');
-    elements.selectCat.classList.remove('is-hidden');
-    elements.catInfo.classList.add('is-hidden');
-    elements.error.classList.add('is-hidden');
+     });
+    refs.selected.classList.remove('hidden');
+    refs.loader.classList.add('hidden');
+        }).catch((error) => {
+        Notiflix.Notify.failure('Error getting data. Please try again later.');
+            refs.loader.classList.add('hidden');
+        }).finally(); 
 
 
-    Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!', {
-        timeout: 4000,
-        position: 'center-center',
-        width: '500px',
-        fontSize: '20px'
+function makeMarkOptions(arr) {
+    return arr.forEach(({name, id}) => {
+    let markUp = `<option value="${id}">${name}</option>`;
+    refs.selected.insertAdjacentHTML('beforeend', markUp);
+     
     });
+      
+};
+
+function makeMarkCard({name, description, temperament}, {url}) {
+    let markUp = `
+    <img src="${url}" alt="${name}" width="460px"/>
+    <div class="descrp-card">
+    <p class="title-card">${name}</p>
+    <p class="description">${description}</p>
+    <p class="temerament">${temperament}</p>
+    </div>`;
+    
+    refs.div.innerHTML = markUp;
 }
